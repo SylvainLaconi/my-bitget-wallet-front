fc90dfb109645a0238efd37bcc871e04e9e1cd8b
Sylvain-xstudio <sylvain.laconi@x-studio.fr>
2025-09-05 14:31:41 +0200

    Private Layout improvement

--- src/layouts/private-layout.tsx
+++ src/layouts/private-layout.tsx
@@ -5,13 +5,13 @@ export default function PrivateLayout() {
   return (
     <div className="flex flex-col h-screen w-screen px-4 pb-12 pt-20 gap-12 relative">
       <header className="fixed top-0 left-0 right-0 px-4 py-4 flex justify-between items-center">
-        <div className="flex items-center gap-2">
+        <div className="flex items-center gap-1">
           <img
             src="/my-bitget-wallet-logo-48x48.png"
             alt="My Bitget Spot Logo"
-            className="w-12 h-12"
+            className="w-10 h-10"
           />
-          <h1 className="text-2xl font-bold text-primary">My Bitget Spot</h1>
+          <h1 className="text-xl font-bold text-primary">My Bitget Spot</h1>
         </div>
         <div className="flex items-center gap-2">
           <LogoutButton />
